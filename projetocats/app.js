c34d28d7b5d44bc837c84cc4bf9b1715e421056b
Érica B Silveira <ericabs2007@gmail.com>
2025-02-10 12:28:28 -0300

    Add files via upload

---
+++ projetocats/app.js
@@ -0,0 +1,57 @@
+//eventos 
+document.addEventListener("DOMContentLoaded", ()=>{
+    const url = ' https://api.thecatapi.com/v1/images/search?limit=10'
+    // console.log(url)
+    fetch(url)
+    .then((response)=>{
+        if(!response.ok){
+            throw new Error ('Erro ao receber dados')
+        }
+        return response.json();
+    }) // faça isso
+
+    .then((data) =>{
+        gerarCategorias(data);
+    }) // depois faça essa
+
+    .catch((err)=>{
+        console.log(err);
+    }) // e se alguma das 2 acima der errado mostre isso 
+
+
+});
+
+const gerarCategorias = (categorias) =>{
+    const select = document.getElementById('select');
+    categorias.map((categoria)=>{
+        const options = document.createElement("option");
+        options.innerHTML = `${categoria}`;
+        options.value = categoria;
+        options.id = categoria ;
+        select.appendChild(options);
+    });
+};
+
+const btnEnviar = document.getElementById('btnEnviar')
+btnEnviar.addEventListener('click', async()=>{
+    const select = document.getElementById('select').value 
+    // console.log(select) 
+    const url = `https://api.chucknorris.io/jokes/random?category=${select}`;
+
+    await fetch(url)
+    .then((response)=>{
+        if(!response.ok){
+            throw new Error ('Error ao buscar piada com categoria')
+        }
+        return response.json()
+    })
+    .then((data) =>{
+        gerarPiada(data);
+    })
+    .catch((err)=>console.log(err)) ;
+});
+
+const gerarPiada = (piada)=>{
+    const nome = document.getElementById('nome')
+    nome.textContent = piada.value
+}
